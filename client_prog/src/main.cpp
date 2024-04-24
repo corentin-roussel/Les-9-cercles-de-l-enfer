d481fa6a9b935c136b33a2d78fc73983f16cce6e
fabien-ricca <fabien.ricca@laplateforme.io>
2024-04-24 10:47:44 +0200

    Modification makefile et structure.

--- client_prog/src/main.cpp
+++ client_prog/src/main.cpp
@@ -1,8 +1,10 @@
 #include "../headers/main.hpp"
+#include "../../utils/headers/utils.hpp"
 
 
 int main()
 {
-    std::cout << "Bonjour, je suis le client" << std::endl;
+    std::cout << "Bonjour, je suis le client et je m'appelle " << toto() << std::endl;
+
     return 0;
 }
